0f571b75912df4572eb1ec9b7ceb5f313a09abe2
Vladimir Zotov <zott14@outlook.com>
2018-05-03 20:45:07 +0300

    S

---
+++ SimpleClassExample/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Car
+    {
+        public string petName;
+        public int currSpeed;
+
+        public void PrintState()
+          => Console.WriteLine("{0} is going {1} KPH.", petName, currSpeed);
+
+        public void SpeedUp(int delta)
+          => currSpeed += delta;
+    }
+}
--- SimpleClassExample/Program.cs
+++ SimpleClassExample/Program.cs
@@ -10,6 +10,21 @@ namespace SimpleClassExample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Class Types *****\n");
+
+            // Allocate and configure Car object.
+            Car myCar = new Car();
+            myCar.petName = "Henry";
+            myCar.currSpeed = 10;
+
+            // Speed up the car a few times and
+            // print out the new state.
+            for (int i = 0; i < 10; i++)
+            {
+                myCar.SpeedUp(5);
+                myCar.PrintState();
+            }
+            Console.ReadKey();
         }
     }
 }
